ae9195049eae4da7d0ad5f307ce24528e0bfb6db
Jon Drew <jonathan@roadmunk.com>
2020-03-09 11:20:58 -0400

    Updated rounding to only include negatives

--- mrr_test.py
+++ mrr_test.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-data = pd.read_csv("mrr.csv")
+# The original dataframe is a table of months and companies, with the companies' MRR per month in each cell.
+df = pd.read_csv("mrr.csv")
 
-print(data.head())
+# Some MRR values have very small negatives. These are rounded up to 0.
+# df = df.round(0)
+
+num = df._get_numeric_data()
+
+num[num < 0] = 0
+
+print(df.head())
