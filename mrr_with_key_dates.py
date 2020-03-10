ef65bddd97abb760d2f97d82aeebca54f87ccad4
Jon Drew <jonathan@roadmunk.com>
2020-03-10 12:11:03 -0400

    Cleaning up comments

--- mrr_with_key_dates.py
+++ mrr_with_key_dates.py
@@ -9,21 +9,12 @@ df = pd.read_csv("key_dates.csv")
 # Remove the last column. It has NaN errors.
 df = df.iloc[:, :-1]
 
-# Convert headers to datetime.
-# month
-# datetime_headers = []
-# for column in list(df.columns.values[5:]):
-#     datetime_headers += dt.strptime(column, '%Y-%M-%d')
-
 # Retention
 
-# cohort_starting_mrr = df.groupby(['2015-01-31']).sum()
 cohort_starting_mrr = df.sum(axis = 0, skipna = True)
 cohort_starting_mrr = cohort_starting_mrr[2:]
 
-# cohort_starting_mrr =  df[[col for col in df.columns if col not in ("Customer Name", "managed?", "first_date", "last_date") 
-#            and dt(col[:3], ) >= dt(2015,1,1) 
-#            and dt(col) <= dt(2016,3,1)]]
+cohort_ending_mrr = df.sum(axis = 0, skipna = True).where(df['2015-01-31'] > 0, 0)
 
 # print(cohort_starting_mrr.head())
-print (cohort_starting_mrr)
+print (cohort_ending_mrr)
