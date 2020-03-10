67f828b6cc5604c8998376fe33a4a258506e780e
Jon Drew <jonathan@roadmunk.com>
2020-03-10 15:52:54 -0400

    Found ending MRR for net retention

--- mrr_with_key_dates.py
+++ mrr_with_key_dates.py
@@ -4,6 +4,7 @@ from datetime import datetime as dt
 import re
 
 # The original dataframe is a table of months and companies, with the companies' MRR per month in each cell.
+# The first 5 fields are non-MRR : 'Row Labels', 'managed?', 'tier', 'first_date', and 'last_date'.
 df = pd.read_csv("key_dates.csv")
 
 # Remove the last column. It has NaN errors.
@@ -11,10 +12,24 @@ df = df.iloc[:, :-1]
 
 # Retention
 
+# Find the total MRR each month. This will be the denominator.
 cohort_starting_mrr = df.sum(axis = 0, skipna = True)
 cohort_starting_mrr = cohort_starting_mrr[2:]
 
-cohort_ending_mrr = df.sum(axis = 0, skipna = True).where(df['2015-01-31'] > 0, 0)
+# Find the MRR 12 months later from companies that made up the denominator. This will be the numerator.
+# df['matches'] = df.apply(lambda x: x['2016-01-31'] if x['2015-01-31'] >= 1 else 0, axis=1)
+# cohort_ending_mrr = df.matches.sum()
+cohort_mrr_12_mths_later = []
+
+for cohort in list(df.columns[17:]):
+    ending_month = pd.to_datetime(np.datetime64(cohort)) + pd.DateOffset(years=1)
+    ending_month_string = ending_month.strftime('%Y-%m-%d')
+    if ending_month < pd.to_datetime(np.datetime64('2019-01-31')):
+        ending_mrr = df.apply(lambda x: x[cohort] if x[ending_month_string] >= 1 else 0, axis=1)
+        sum_of_ending_mrr = ending_mrr.sum()
+        mrr_object = {cohort: ending_mrr}
+        cohort_mrr_12_mths_later.append(mrr_object)
+
 
 # print(cohort_starting_mrr.head())
-print (cohort_ending_mrr)
+print (cohort_mrr_12_mths_later)
