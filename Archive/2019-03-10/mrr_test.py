537b102ab4d41586d6b8e7563d540bc19983e16d
Jon Drew <jonathan@roadmunk.com>
2020-03-10 09:11:35 -0400

    Archived 2019-03-10. Starting new version with key dates.

---
+++ Archive/2019-03-10/mrr_test.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+# The original dataframe is a table of months and companies, with the companies' MRR per month in each cell.
+df = pd.read_csv("mrr.csv")
+
+# For now, all numbers are rounded to avoid very small negative values, e.g. -0.01.
+df = df.round(0)
+
+#  WIP: Round only values that are less than 0.
+# num = df._get_numeric_data()
+# num[num < 0] = 0
+
+# Finding the first date a company produced MRR.
+# first_date = df.keys()[np.argmax(df.values!=0,axis=1)]
+
+# no_names = df.loc[:, 2:]
+# first_date = no_names.keys()[np.argmax(no_names.values!=0,axis=1)]
+# df.insert(2, "first_date", first_date)
+
+no_names = df.drop(['Customer Name', 'Self-serve'], axis=1)
+first_date = no_names.keys()[np.argmax(no_names.values!=0,axis=1)]
+df.insert(2, "first_date", first_date)
+
+print(df.head())
