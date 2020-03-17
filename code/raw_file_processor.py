8cf9151b44a31b24df63d4280fb8aa45bdc4a370
Jon Drew <jonathan@roadmunk.com>
2020-03-17 12:59:21 -0400

    Separated valid and invalid subscriptions.

--- code/raw_file_processor.py
+++ code/raw_file_processor.py
@@ -6,6 +6,8 @@ import re
 # This file contains all of the subscriptions as they are provided by the input system.
 df = pd.read_csv("schema/subscriptions_raw.csv")
 
+error_df = df.loc[df.isna().any(axis=1)]
 
+df_for_processing = df.loc[df.notna().all(axis=1)]
 
-print (df)
+print (error_df, df_for_processing)
