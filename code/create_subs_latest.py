f0585a6da95de12b3f6a966370ff9eb0cf668b0b
Jon Drew <jonathan@roadmunk.com>
2020-03-18 14:29:12 -0400

    Final commit to raw_subs.

--- code/create_subs_latest.py
+++ code/create_subs_latest.py
@@ -9,10 +9,17 @@ import os
 # Get the file containing the latest output of raw_file_processor.py
 list_of_subs_files = glob.glob('schema/*.csv')
 latest_subs_file = max(list_of_subs_files, key=os.path.getctime)
-df = pd.read_csv(latest_subs_file)
-df['analysis_datetime'] = dt.now().strftime("%Y-%m-%d_%I-%M-%S_%p")
+subs_df = pd.read_csv(latest_subs_file)
+
+# Add an analysis_datetime column with the current datetime.
+subs_df['analysis_datetime'] = dt.now().strftime("%Y-%m-%d_%I-%M-%S_%p")
+
+
+
+# Add gross and net MRR/ARR columns based on products.
+
 
 latest_subs_path = "schema/subscriptions_latest_" + dt.now().strftime("%Y-%m-%d_%I-%M-%S_%p") + ".csv"
-df.to_csv(latest_subs_path, index=False)
+subs_df.to_csv(latest_subs_path, index=False)
 
-print(latest_subs_file)
+print(subs_df)
