bd35eb26eef6f5d4a8dcbfd7dc1b4cdf12c865dc
Jon Drew <jonathan@roadmunk.com>
2020-03-31 09:22:22 -0400

    Completed MRR calculation.

--- code/create_subs_latest.py
+++ code/create_subs_latest.py
@@ -22,20 +22,51 @@ subs_bundles_df = pd.merge(latest_subs_df, bundle_df, on='bundle_id')
 # Join subs_bundles and products for subs_products.
 products_df = pd.read_csv('schema/products.csv')
 subs_products_df = pd.merge(subs_bundles_df, products_df, on='product_id')
-subs_products_df = subs_products_df.groupby('RMID')['annual_unit_price', 'monthly_unit_price'].sum()
+subs_products_df = subs_products_df.groupby('RMID')[['annual_unit_price', 'monthly_unit_price']].sum()
 
-# Write the MRR and ARR to columns in the latest_subs_df
+# Merge the MRR and ARR into the latest_subs_df
 latest_subs_df = pd.merge(latest_subs_df, subs_products_df, on='RMID')
+latest_subs_df.rename(columns={'annual_unit_price': 'arr', 'monthly_unit_price': 'mrr'}, inplace=True)
 
 # This section deals with movement classification.
 # Join accounts and logos for accounts_logos
-# accounts_df = pd.read_csv('schema/accounts.csv')
-# logos_df = pd.read_csv('schema/logos.csv')
-# accounts_logos_df = pd.merge(accounts_df, logos_df, on='logo_id')
+accounts_df = pd.read_csv('schema/accounts.csv')
+logos_df = pd.read_csv('schema/logos.csv')
+accounts_logos_df = pd.merge(accounts_df, logos_df, on='logo_id')
 
-# Classifications are done from most general to most specific, so that the general case can be overwritten.
+# Add the relevant columns to latest_subs_df.
+latest_subs_df['logo_start'] = accounts_logos_df['first_start_of_term_y']
+latest_subs_df['logo_end'] = accounts_logos_df['last_end_of_term_y']
+latest_subs_df['last_gross_mrr'] = accounts_logos_df['gross_mrr_y']
+latest_subs_df['last_gross_arr'] = accounts_logos_df['gross_arr_y']
+latest_subs_df['last_net_mrr'] = accounts_logos_df['net_mrr_y']
+latest_subs_df['last_net_arr'] = accounts_logos_df['net_arr_y']
+
+'''
+Assigning the classification is done from least to most precise.
+E.g. churning is a form of contraction ($1 -> $0), so first the subscription
+is classified as contraction, then the classification is overwritten as a churn.
+'''
+
+# Is it an increase since the last subscriptions (expansion)?
+latest_subs_df.loc[(latest_subs_df['mrr'] > latest_subs_df['last_gross_mrr'])
+    , 'movement_classification'] = "expansion"
+
+# Is it a decrease since the last subscriptions (contraction)?
+latest_subs_df.loc[(latest_subs_df['mrr'] < latest_subs_df['last_gross_mrr'])
+    , 'movement_classification'] = "contraction"
+
+# Is it the same as the last subscriptions (stable)?
+latest_subs_df.loc[(latest_subs_df['mrr'] == latest_subs_df['last_gross_mrr'])
+    , 'movement_classification'] = "stable"
 
 # Is it the first time this logo has a subscription (new)?
+latest_subs_df.loc[(latest_subs_df['start_of_term'] == latest_subs_df['logo_start'])
+    , 'movement_classification'] = "new"
+
+# # Is it the last time this logo has a subscription (churn)?
+latest_subs_df.loc[(latest_subs_df['end_of_term'] == latest_subs_df['logo_end'])
+    , 'movement_classification'] = "churn"
 
 latest_subs_path = "schema/subscriptions_latest/subscriptions_latest_" + dt.now().strftime("%Y-%m-%d_%I-%M-%S_%p") + ".csv"
 latest_subs_df.to_csv(latest_subs_path, index=False)
